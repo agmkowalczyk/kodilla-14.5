98fce88cf7ec74f3e7085471945dee5af459d939
agmkowalczyk <agm.kowalczyk@gmail.com>
2017-11-15 02:43:37 +0100

    ReactJS - Contacts - JSX

--- components/Contact.js
+++ components/Contact.js
@@ -5,18 +5,14 @@ var Contact = React.createClass({
 
   render: function() {
     return (
-      React.createElement('div', {className: 'contactItem'},
-        React.createElement('img', {
-          className: 'contactImage',
-          src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'
-        }),
-        React.createElement('div', {className: 'contactDesc'},
-          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-          React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-          this.props.item.email)
-        )
-      )
+      <div className={'contactItem'}>
+        <img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'} /> 
+        <div className={'contactDesc'}>   
+          <p className={'contactLabel'}> Imię: {this.props.item.firstName} </p>
+          <p className={'contactLabel'}>Nazwisko: {this.props.item.lastName}</p>
+          <a href={'maito:' + this.props.item.email} className={'contactEmail'}>Imię: {this.props.item.email}</a>
+        </div>
+      </div>    
     )
   },
 });
